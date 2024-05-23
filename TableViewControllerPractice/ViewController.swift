de07786d173daeaee6741fbd0f1ac6e3926b4867
sixteenis <psm0103907@naver.com>
2024-05-23 16:35:10 +0900

    Initial Commit

---
+++ TableViewControllerPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewControllerPractice
+//
+//  Created by 박성민 on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
